a499bb23e51ec9c838332fe4aa43b16f7ba02264
Brian <mahuibrian14@gmail.com>
2023-06-09 16:43:04 +0300

    presentability 1

--- React-frontend/src/components/AppointmentItem.jsx
+++ React-frontend/src/components/AppointmentItem.jsx
@@ -1,16 +1,18 @@
 function AppointmentItem({ appointment }) {
-    return (
-      <div>
-        <h3>ID: {appointment.id}</h3>
+    /**<h3>ID: {appointment.id}</h3>
         <p>Doctor ID: {appointment.doctor_id}</p>
         <p>Patient ID: {appointment.patient_id}</p>
-        <p>Appointment Date: {appointment.appointment_date}</p>
-        <p>Duration: {appointment.duration}</p>
         <p>Created At: {appointment.created_at}</p>
-        <p>Updated At: {appointment.updated_at}</p>
+        <p>Updated At: {appointment.updated_at}</p> */
+    return (
+     <> 
+      <div>
         <p>Patient Name: {appointment.patient_name}</p>
         <p>Doctor Name: {appointment.doctor_name}</p>
+        <p>Appointment Date: {appointment.appointment_date}</p>
+        <p>Duration: {appointment.duration}</p>
       </div>
+     </>
     );
   }
   
--- React-frontend/src/components/PatientItem.jsx
+++ React-frontend/src/components/PatientItem.jsx
@@ -1,13 +1,15 @@
 function PatientItem({ patient }) {
+    /*<h3>ID: {patient.id}</h3>
+      <p>Created At: {patient.created_at}</p>
+      <p>Updated At: {patient.updated_at}</p>*/
     return (
       <div key={patient.id}>
-        <h3>ID: {patient.id}</h3>
+        
         <p>Name: {patient.name}</p>
         <p>Age: {patient.age}</p>
         <p>Condition: {patient.condition}</p>
         <p>Sex: {patient.sex}</p>
-        <p>Created At: {patient.created_at}</p>
-        <p>Updated At: {patient.updated_at}</p>
+        
       </div>
     );
   }
