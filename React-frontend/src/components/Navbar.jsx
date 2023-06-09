9757d667be06883b901aa0cb1acfb909db2875b9
Brian <mahuibrian14@gmail.com>
2023-06-09 19:41:07 +0300

    Navbar styling

--- React-frontend/src/components/Navbar.jsx
+++ React-frontend/src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
 
+
 function Navbar() {
   return (
-    <div className='container'>
-      <div className='navbar'>
+    <div className="navbar-container">
+      <div className="navbar">
         <div>
           <h2>Healthcare Made Easy</h2>
         </div>
 
-        <div className='nav-links'>
-          <Link to='/doctors'>Doctors</Link>
-          <Link to='/patient-list'>Patients</Link>
-          <Link to='/appointment'>Appointment</Link>
+        <div className="nav-links">
+          <Link to="/doctors">Doctors</Link>
+          <Link to="/patient-list">Patients</Link>
+          <Link to="/appointment">Appointment</Link>
         </div>
       </div>
     </div>
