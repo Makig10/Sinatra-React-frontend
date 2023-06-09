392b7e2c384b70c0f55a713f47631f11cbaf3e5b
Brian <mahuibrian14@gmail.com>
2023-06-09 14:29:34 +0300

    POST

--- React-frontend/src/components/AppointmentListandForm.jsx
+++ React-frontend/src/components/AppointmentListandForm.jsx
@@ -1,11 +1,50 @@
-import AppointmentItem from "./AppointmentItem";
-function AppointmentListAndForm({appointments}){
+import { useState } from 'react';
+
+function AppointmentListAndForm({ appointments }) {
+  const [patientName, setPatientName] = useState('');
+  const [doctorName, setDoctorName] = useState('');
+
+  const handlePatientNameChange = (e) => {
+    setPatientName(e.target.value);
+  };
+
+  const handleDoctorNameChange = (e) => {
+    setDoctorName(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Create a new appointment object
     const newAppointment = {
-        patientName: patientName,
-        doctorName: doctorName,
-      };
-      return(
-        <div>
+      name: patientName,
+      doctor_name: doctorName,
+    };
+
+    // Send POST request to book appointment
+    fetch('/book_appointment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newAppointment),
+    })
+      .then((response) => response.json())
+      .then((appointment) => {
+        // Add the new appointment to the list
+        setAppointments([...appointments, appointment]);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+    // Reset input fields
+    setPatientName('');
+    setDoctorName('');
+  };
+
+  return (
+    <div>
       <h2>Appointment List</h2>
 
       <form onSubmit={handleFormSubmit}>
@@ -41,6 +80,3 @@ function AppointmentListAndForm({appointments}){
 }
 
 export default AppointmentListAndForm;
-      
-  
-
